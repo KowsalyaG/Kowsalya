44fd129b7bf51f8948f7daa814e3dfaa1982f56d
KowsalyaG <kowsalyabhairavi@gmail.com>
2016-06-06 21:44:12 +0530

    Rename numb.c to n.c

---
+++ n.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d",&num);
+	if(num>0){
+		printf("POSITIVE");
+	}
+	if(num<0){
+		printf("NEGATIVE");
+	}
+	if(num<0){
+		printf("NEGATIVE");
+	}
+	if(num==0){
+		printf("ZERO");
+	}
+	return 0;
+}
