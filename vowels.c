d66b2d13dabd3a045580e0be7ca0bec74780707c
KowsalyaG <kowsalyabhairavi@gmail.com>
2016-06-07 11:47:50 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+char letter;
+scanf("%c",&letter);
+if(letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter=='u'){
+printf("VOWEL");
+else
+printf("CONSONANT");
+return 0;
+}
