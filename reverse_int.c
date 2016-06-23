b1cf3e34481e41c69f8455932ba750f84c026c6f
KowsalyaG <kowsalyabhairavi@gmail.com>
2016-06-23 09:53:16 +0530

    Update reverse_int.c

--- reverse_int.c
+++ reverse_int.c
@@ -3,8 +3,12 @@
 int main()
 {
     long long int a,b,rem[1000000];
-    long long int i,j=0,len=1;
+    long long int flag=0,i,j=0,len=1;
     scanf("%lld",&a);
+    if(a<0){
+        a=-1*a;
+        flag=1;
+    }
     while(a>0){
         rem[j++]=a%10;
         a=a/10;
@@ -12,6 +16,11 @@ int main()
     for(i=j-1;i>=0;i--,len=len*10){
         b=b+rem[i]*len;
     }
+    if(flag==0)
     printf("%d",b);
+    if(flag==1){
+    b=-1*b;
+    printf("%d",b);
+    }
     return 0;
 }
