4596c50a44f1f0d50af1a3a3dc5fb84e7cfdf97b
KowsalyaG <kowsalyabhairavi@gmail.com>
2016-06-23 10:04:55 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int fact(int n){
+    if(n==0)
+    return 1;
+    else{
+        return n*fact(n-1);
+    }
+}
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    printf("%d",fact(num));
+    return 0;
+}
