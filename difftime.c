4465b444ad4102ef65c9922b21a44670647a7111
KowsalyaG <kowsalyabhairavi@gmail.com>
2017-09-14 20:07:54 +0530

    Update difftime.c

--- difftime.c
+++ difftime.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <math.h>
 int main()
 {
 int hr1,hr2,min1,min2,time1,time2,min,sec;
@@ -8,7 +8,7 @@ scanf("%d %d",&hr1,&min1);
 scanf("%d %d",&hr2,&min2);
 time1=(hr1*60)+min1;
 time2=(hr2*60)+min2;
-min=time2-time1;
+min=abs(time2-time1);
 sec=min*60;
 printf("minutes between%d:%d and %d:%d is %d",hr1,min1,hr2,min2,min);
 printf("\nseconds between%d:%d and %d:%d is %d",hr1,min1,hr2,min2,sec);
