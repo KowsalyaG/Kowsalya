014738ec539a860926352ec3d087bba8c7c51931
KowsalyaG <kowsalyabhairavi@gmail.com>
2017-09-25 23:50:02 +0530

    Update findDuplicate.c

--- findDuplicate.c
+++ findDuplicate.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 #include <stdlib.h>
 int main(){
-    int i,arr[1000],len,max=-1,*hash;
+    int i,*arr,len,max=-1,*hash;
     scanf("%d",&len);
+    
+    //Dynamically allocating memory for array
+    arr = (int*) malloc(len * sizeof(int));
+    
     for(i=0;i<len;i++){
         scanf("%d",&arr[i]);
         // To find the largest number in array
         if(max<arr[i])
             max=arr[i];
     }
+    
     //Dynamically allocating memory for hash map
     hash = (int*) malloc(max * sizeof(int));
     
