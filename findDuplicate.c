6cd6568db118a7b1efd1e7ba4bd339cf8a472d18
KowsalyaG <kowsalyabhairavi@gmail.com>
2017-09-25 23:48:01 +0530

    Create findDuplicate.c

---
+++ findDuplicate.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main(){
+    int i,arr[1000],len,max=-1,*hash;
+    scanf("%d",&len);
+    for(i=0;i<len;i++){
+        scanf("%d",&arr[i]);
+        // To find the largest number in array
+        if(max<arr[i])
+            max=arr[i];
+    }
+    //Dynamically allocating memory for hash map
+    hash = (int*) malloc(max * sizeof(int));
+    
+    //Traversing the array and using hash map, finding the duplicates in the array
+    for(i=0;i<len;i++){
+        if(hash[arr[i]]>=0)
+        hash[arr[i]]++;
+        if(hash[arr[i]]>1){
+            printf("%d is duplicated\n",arr[i]);
+            hash[arr[i]]=-1;
+        }
+    }
+}
