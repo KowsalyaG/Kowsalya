5f49cb428e53a010d650a766eca55a32afc90f51
KowsalyaG <kowsalyabhairavi@gmail.com>
2017-10-19 18:20:12 +0530

    Create findNth.c

---
+++ findNth.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int n,k=1,a[10000],i;
+a[0]=3,a[1]=4;
+scanf("%d",&n);
+for(i=0;i<n;i++){
+a[++k]=(a[i]*10)+3;
+a[++k]=(a[i]*10)+4;
+}
+printf("%d",a[n-1]);
+return 0;
+}
